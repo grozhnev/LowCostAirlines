db3c8bd7e1420dbe386880566dedc858425e9ff9
klysov <thereis9000@gmail.com>
2017-10-04 12:27:50 +0300

    added: sql-script for filling database with data

---
+++ Database_scripts/fill_tables.sql
@@ -0,0 +1,34 @@
+/*==============================================================*/
+/*                  Filling tables with data                    */
+/*==============================================================*/
+
+INSERT INTO Plane (idPlane, MaxLoad, CurrentLoad)
+ VALUES('1','50','0');
+INSERT INTO Plane (idPlane, MaxLoad, CurrentLoad)
+ VALUES('2','50','0');
+INSERT INTO Plane (idPlane, MaxLoad, CurrentLoad)
+ VALUES('3','50','0');
+ 
+INSERT INTO Airport (idAirport, Name)
+ VALUES('1','Berlin');
+INSERT INTO Airport (idAirport, Name)
+ VALUES('2','London');
+INSERT INTO Airport (idAirport, Name)
+ VALUES('3','Paris');
+INSERT INTO Airport (idAirport, Name)
+ VALUES('4','Rome');
+ 
+INSERT INTO Customer (idCustomer, isAdmin, Name, LastName, PersonID)
+ VALUES('1','0', 'Jack', 'Jackson', '123-123-123');
+INSERT INTO Customer (idCustomer, isAdmin, Name, LastName, PersonID)
+ VALUES('2','0', 'John', 'Johnson', '321-321-321');
+ 
+INSERT INTO Flight (idFlight, DateTime, Airport_Source, Airport_Destination, idPlane)
+ VALUES('1','2015-11-05 14:29:36', '1', '2', '1');
+INSERT INTO Flight (idFlight, DateTime, Airport_Source, Airport_Destination, idPlane)
+ VALUES('2','2015-11-06 13:03:21', '3', '4', '2');
+ 
+INSERT INTO Ticket (idTicket, idFlight, idCustomer, Price, LuggagePrice, RegistrationPriority)
+ VALUES('1','1', '1', '100', '10', '1');
+INSERT INTO Ticket (idTicket, idFlight, idCustomer, Price, LuggagePrice, RegistrationPriority)
+ VALUES('2','2', '2', '50', '100', '0');
