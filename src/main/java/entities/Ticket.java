3c91b13f7269d50477ee2d335952f77ebc1f6cc0
Artem.K <kuznecov_15@mail.ru>
2017-10-05 18:56:39 +0300

    Added DAO Class Ticket implementation. The field of class Ticket were created and getters and setters for them.

--- src/main/java/entities/Ticket.java
+++ src/main/java/entities/Ticket.java
@@ -1,4 +1,62 @@
 package entities;
 
 public class Ticket {
+
+    private int idTicket;
+    private int idFlight;
+    private int idCustomer;
+    private int Price;
+    private int LuggagePrice;
+    private boolean RegistrationPriority;
+
+    public int getIdTicket() {
+        return idTicket;
+    }
+
+    public void setIdTicket(int idTicket) {
+        this.idTicket = idTicket;
+    }
+
+    public void setIdFlight(int idFlight) {
+        this.idFlight = idFlight;
+    }
+
+    public void setIdCustomer(int idCustomer) {
+        this.idCustomer = idCustomer;
+    }
+
+    public void setPrice(int price) {
+        Price = price;
+    }
+
+    public void setLuggagePrice(int luggagePrice) {
+        LuggagePrice = luggagePrice;
+    }
+
+    public void setRegistrationPriority(boolean registrationPriority) {
+        RegistrationPriority = registrationPriority;
+    }
+
+    public int getIdFlight() {
+
+        return idFlight;
+    }
+
+    public int getIdCustomer() {
+        return idCustomer;
+    }
+
+    public int getPrice() {
+        return Price;
+    }
+
+    public int getLuggagePrice() {
+        return LuggagePrice;
+    }
+
+    public boolean isRegistrationPriority() {
+        return RegistrationPriority;
+    }
 }
+
+
