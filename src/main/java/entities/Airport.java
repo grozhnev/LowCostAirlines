065baa1f917fa712dfbe5b165c0a06c20fb2dc98
klysov <thereis9000@gmail.com>
2017-10-06 09:33:20 +0300

    implemented airport and plane entities

--- src/main/java/entities/Airport.java
+++ src/main/java/entities/Airport.java
@@ -1,5 +1,30 @@
 package entities;
 
 public class Airport {
+    private int airportId;
+    private String Name;
 
+    public int getAirportId() {
+        return airportId;
+    }
+
+    public void setAirportId(int airportId) {
+        this.airportId = airportId;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Airport{" +
+                "airportId=" + airportId +
+                ", Name='" + Name + '\'' +
+                '}';
+    }
 }
--- src/main/java/entities/Plane.java
+++ src/main/java/entities/Plane.java
@@ -1,4 +1,40 @@
 package entities;
 
 public class Plane {
+    private int planeId;
+    private int maxLoad;
+    private int currentLoad;
+
+    public int getPlaneId() {
+        return planeId;
+    }
+
+    public void setPlaneId(int planeId) {
+        this.planeId = planeId;
+    }
+
+    public int getMaxLoad() {
+        return maxLoad;
+    }
+
+    public void setMaxLoad(int maxLoad) {
+        this.maxLoad = maxLoad;
+    }
+
+    public int getCurrentLoad() {
+        return currentLoad;
+    }
+
+    public void setCurrentLoad(int currentLoad) {
+        this.currentLoad = currentLoad;
+    }
+
+    @Override
+    public String toString() {
+        return "Plane{" +
+                "planeId=" + planeId +
+                ", maxLoad=" + maxLoad +
+                ", currentLoad=" + currentLoad +
+                '}';
+    }
 }
