128a984c7e855647e3013feae53fec7aa31cc9e2
klysov <thereis9000@gmail.com>
2017-10-06 12:41:12 +0300

    added: DAO implementation for airport and plane

--- src/main/java/dao/AirportDaoImpl.java
+++ src/main/java/dao/AirportDaoImpl.java
@@ -1,4 +1,113 @@
 package dao;
 
-public class AirportDaoImpl {
+import entities.Airport;
+import services.ConnectionFactory;
+
+import java.sql.*;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Implementation of airport DAO
+ * @author klysov
+ */
+public class AirportDaoImpl implements AirportDao{
+    @Override
+    public Airport extractAirportFromResultSet(ResultSet rs) throws SQLException {
+        Airport airport = new Airport();
+        airport.setIdAirport( rs.getInt("idAirport") );
+        airport.setName( rs.getString("Name") );
+        return airport;
+    }
+
+    @Override
+    public Set<Airport> getAllAirports() {
+        Connection connection = ConnectionFactory.getConnection();
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Airport");
+            Set<Airport> airports = new HashSet<>();
+            while(rs.next())
+            {
+                Airport airport = extractAirportFromResultSet(rs);
+                airports.add(airport);
+            }
+            return airports;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public Airport getAirportById(int id) {
+        Connection connection = ConnectionFactory.getConnection();
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Airport WHERE idAirport=" + id);
+            if(rs.next())
+            {
+                Airport airport = extractAirportFromResultSet(rs);
+                return airport;
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public boolean insertAirport(Airport airport) {
+        Connection connection = ConnectionFactory.getConnection();
+        try {
+            PreparedStatement ps = connection.prepareStatement("INSERT INTO Airport VALUES (NULL, ?, ?, ?)");
+            ps.setString(1, user.getName());
+            ps.setString(2, user.getPass());
+            ps.setInt(3, user.getAge());
+            int i = ps.executeUpdate();
+            if(i == 1) {
+                return true;
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean updateAirport(Airport airport) {
+        Connector connector = new Connector();
+        Connection connection = connector.getConnection();
+        try {
+            PreparedStatement ps = connection.prepareStatement("UPDATE user SET name=?, pass=?, age=? WHERE id=?");
+            ps.setString(1, user.getName());
+            ps.setString(2, user.getPass());
+            ps.setInt(3, user.getAge());
+            ps.setInt(4, user.getId());
+            int i = ps.executeUpdate();
+            if(i == 1) {
+                return true;
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean deleteAirport(Airport airport) {
+        Connector connector = new Connector();
+        Connection connection = connector.getConnection();
+        try {
+            Statement stmt = connection.createStatement();
+            int i = stmt.executeUpdate("DELETE FROM user WHERE id=" + id);
+            if(i == 1) {
+                return true;
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+    }
 }
--- src/main/java/dao/PlaneDaoImpl.java
+++ src/main/java/dao/PlaneDaoImpl.java
@@ -1,4 +1,123 @@
 package dao;
 
-public class PlaneDaoImpl {
+import entities.Plane;
+import services.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Set;
+
+/**
+ * Implementation of plane DAO
+ * @author klysov
+ */
+public class PlaneDaoImpl implements PlaneDao {
+    @Override
+    public Plane extractPlaneFromResultSet(ResultSet rs) throws SQLException {
+        User user = new User();
+        user.setId( rs.getInt("id") );
+        user.setName( rs.getString("name") );
+        user.setPass( rs.getString("pass") );
+        user.setAge( rs.getInt("age") );
+        return user;
+    }
+
+    @Override
+    public Set<Plane> getAllPlanes() {
+        Connector connector = new Connector();
+        Connection connection = connector.getConnection();
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM user");
+            Set users = new HashSet();
+            while(rs.next())
+            {
+                User user = extractUserFromResultSet(rs);
+                users.add(user);
+            }
+            return users;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public Plane getPlaneById(int id) {
+        Connection connection = ConnectionFactory.getConnection();
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM user WHERE id=" + id);
+            if(rs.next())
+            {
+                Plane plane = new Plane();
+                plane.setId( rs.getInt("id") );
+                plane.setName( rs.getString("name") );
+                plane.setPass( rs.getString("pass") );
+                plane.setAge( rs.getInt("age") );
+                return plane;
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public boolean insertPlane(Plane plane) {
+        Connector connector = new Connector();
+        Connection connection = connector.getConnection();
+        try {
+            PreparedStatement ps = connection.prepareStatement("INSERT INTO user VALUES (NULL, ?, ?, ?)");
+            ps.setString(1, user.getName());
+            ps.setString(2, user.getPass());
+            ps.setInt(3, user.getAge());
+            int i = ps.executeUpdate();
+            if(i == 1) {
+                return true;
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean updatePlane(Plane plane) {
+        Connector connector = new Connector();
+        Connection connection = connector.getConnection();
+        try {
+            PreparedStatement ps = connection.prepareStatement("UPDATE user SET name=?, pass=?, age=? WHERE id=?");
+            ps.setString(1, user.getName());
+            ps.setString(2, user.getPass());
+            ps.setInt(3, user.getAge());
+            ps.setInt(4, user.getId());
+            int i = ps.executeUpdate();
+            if(i == 1) {
+                return true;
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean deletePlane(Plane plane) {
+        Connector connector = new Connector();
+        Connection connection = connector.getConnection();
+        try {
+            Statement stmt = connection.createStatement();
+            int i = stmt.executeUpdate("DELETE FROM user WHERE id=" + id);
+            if(i == 1) {
+                return true;
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+    }
 }
