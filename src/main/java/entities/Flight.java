2e181f13f16e57b1bd1c5e458638a9b1bc404192
Georgii Rozhnev <georgii_rozhnev@epam.com>
2017-10-09 19:16:42 +0300

    Fill in class Flight with fields and getters/setters. Going to pull updates from master.

--- src/main/java/entities/Flight.java
+++ src/main/java/entities/Flight.java
@@ -1,4 +1,62 @@
 package entities;
 
+import java.time.*;
+
 public class Flight {
+
+    /* model:
+
+     CREATE TABLE Flight
+    (
+       idFlight             INT NOT NULL AUTO_INCREMENT,
+       DateTime             DATETIME(6) NOT NULL,
+       Airport_Source       INT NOT NULL,
+       Airport_Destination  INT NOT NULL,
+       idPlane              INT NOT NULL,
+       PRIMARY KEY (idFlight)
+    );*/
+
+
+
+    private int idFlight;
+    private LocalDateTime dateTime;
+    private String airportDeparture;
+    private String airportArrival;
+    private int planeId;
+
+
+    public int getIdFlight() {
+        return idFlight;
+    }
+    public void setIdFlight(int idFlight) {
+        this.idFlight = idFlight;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getAirportDeparture() {
+        return airportDeparture;
+    }
+    public void setAirportDeparture(String airportDeparture) {
+        this.airportDeparture = airportDeparture;
+    }
+
+    public String getAirportArrival() {
+        return airportArrival;
+    }
+    public void setAirportArrival(String airportArrival) {
+        this.airportArrival = airportArrival;
+    }
+
+    public int getPlaneId() {
+        return planeId;
+    }
+    public void setPlaneId(int planeId) {
+        this.planeId = planeId;
+    }
 }
