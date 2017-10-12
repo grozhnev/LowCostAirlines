c06ac5d2128b951b21800cc7890b39b9839b29b1
Kirill Lysov <thereis9000@gmail.com>
2017-10-12 11:13:52 +0300

    added working dropdown list for airport

--- src/main/java/controllers/TicketManagementController.java
+++ src/main/java/controllers/TicketManagementController.java
@@ -1,4 +1,27 @@
 package controllers;
 
-public class TicketManagementController {
+import dao.AirportDAO;
+import entities.Airport;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Set;
+
+@WebServlet("/ticketmanagement")
+public class TicketManagementController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Set<Airport> airports = new AirportDAO().getAll();
+            request.setAttribute("airports", airports);
+            request.getRequestDispatcher("/ticketmanagement.jsp").forward(request, response);
+        } catch (SQLException e) {}
+    }
+
 }
--- src/main/webapp/ticketmanagement.jsp
+++ src/main/webapp/ticketmanagement.jsp
@@ -1,9 +1,14 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
     <title>Title</title>
 </head>
 <body>
-
+<select name="airport">
+    <c:forEach items="${airports}" var="airport">
+        <option value="${airport.airportId}"><c:out value="${airport.name}" /></option>
+    </c:forEach>
+</select>
 </body>
 </html>
