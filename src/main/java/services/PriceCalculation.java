7802e720e4f94c16287bfd9df77c8176e2713d54
Georgii Rozhnev <georgii_rozhnev@epam.com>
2017-10-16 20:48:36 +0300

    Price calculation method completed.

---
+++ src/main/java/services/PriceCalculation.java
@@ -0,0 +1,49 @@
+package services;
+
+import lombok.Data;
+
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+@Data
+public class PriceCalculation {
+    private static Long basicPrice = 100L;
+    private static Long basicLuggageFee = 50L;
+    private static Long approachingDepartureDailyFee = 35L;
+    private static Long luggagePricePerKilo = 5L;
+
+    private Long price;
+    private Long timeBeforeDeparture;
+    private Long daysBeforeDeparture;
+
+    public Long calculatePrice(Long inputPrice, Date departureDate, Boolean priority,
+                               Double planeLoadRate, Long luggageWeight) throws SQLException {
+
+        Long ticketPrice = inputPrice;
+
+        Date currentDate = new Timestamp(System.currentTimeMillis());
+
+        timeBeforeDeparture = departureDate.getTime() - currentDate.getTime();
+
+        if (timeBeforeDeparture <= 0L) {
+            System.err.println("Departure time you've chosen has already passed! \nPlease, select valid date.");
+            throw new IllegalArgumentException();
+        } else {
+            daysBeforeDeparture = TimeUnit.MILLISECONDS.toDays(timeBeforeDeparture);
+            if (priority) {
+                ticketPrice *= 2;
+            }
+            if (luggageWeight >= 0L) {
+                ticketPrice += luggagePricePerKilo * luggageWeight;
+            }
+            if (daysBeforeDeparture <= 14L) {
+                ticketPrice += daysBeforeDeparture * approachingDepartureDailyFee;
+            }
+            ticketPrice *= Math.round(planeLoadRate * 100);
+        }
+        return ticketPrice;
+    }
+    
+}
