5ebf8c06c8f729520029a04160c7031e00fe7dd3
Kirill Lysov <thereis9000@gmail.com>
2017-10-18 14:08:03 +0300

    fixed security

--- src/main/java/loginlogout/LoginServlet.java
+++ src/main/java/loginlogout/LoginServlet.java
@@ -1,64 +1,39 @@
 package loginlogout;
 
 import dao.CustomerDAO;
+import dao.DAO;
 import entities.Customer;
-import org.apache.log4j.Logger;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.SQLException;
+import java.util.Set;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
 
 public class LoginServlet extends HttpServlet {
 
-    static final Logger LOGGER = Logger.getLogger(LoginServlet.class);
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-       //req.getRequestDispatcher("login.jsp").forward(req, resp);
-        LOGGER.info("Login page");
-    }
+    private DAO<Customer> customerDAO = new CustomerDAO();
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        request.getRequestDispatcher("login.jsp").include(request, response);
 
+        request.getRequestDispatcher("login.jsp").include(request, response);
         String email = request.getParameter("email");
         String password = request.getParameter("password");
-        LOGGER.info("Got parameters: email=" + email + " password=" + password);
 
         try {
-            Customer customer = new CustomerDAO()
-                    .getAll()
-                    .stream()
-                    .filter(o -> o.getEmail().equals(email) && o.getPassword().equals(password))
-                    .findFirst()
-                    .get();
-            if (customer != null) {
-                LOGGER.info("Got customer=" + customer);
-                out.print("Welcome, " + email);
-                HttpSession session = request.getSession();
-                session.setAttribute("email", email);
-                session.setMaxInactiveInterval(15);
-                Cookie userName = new Cookie("email", email);
-                response.addCookie(userName);
-                LOGGER.info("Added cookie="+userName);
-
-                LOGGER.info("Redirecting to /ticketmanagement");
-                response.sendRedirect("/ticketmanagement");
-            } else {
-                response.sendRedirect("/");
+            Set<Customer> customerSet = customerDAO.getAll();
+            for (Customer customer : customerSet) {
+                if (email.equals(customer.getEmail()) && password.equals(customer.getPassword())) {
+                    Cookie userName = new Cookie("email", email);
+                    response.addCookie(userName);
+                    response.sendRedirect("/ticketmanagement");
+                }
             }
         } catch (SQLException e) {
-            LOGGER.warn(e);
+            e.printStackTrace();
         }
-
-        out.close();
     }
 }
--- src/main/java/loginlogout/LogoutServlet.java
+++ src/main/java/loginlogout/LogoutServlet.java
@@ -1,7 +1,5 @@
 package loginlogout;
 
-import org.apache.log4j.Logger;
-
 import java.io.IOException;
 
 import javax.servlet.ServletException;
@@ -15,35 +13,23 @@ import javax.servlet.http.HttpSession;
 /**
  * Servlet implementation class LogoutServlet
  */
+
 public class LogoutServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-    static final Logger LOGGER = Logger.getLogger(LogoutServlet.class);
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        req.getRequestDispatcher("logout.jsp").include(req, resp);
+
+        Cookie cookie = new Cookie("email", "");
+        cookie.setMaxAge(0);
+        resp.addCookie(cookie);
+        resp.sendRedirect("/");
+
+    }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        super.doGet(req, resp);
-    }
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html");
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("JSESSIONID")) {
-                    LOGGER.info("JSESSIONID=" + cookie.getValue());
-                    break;
-                }
-            }
-        }
-        //invalidate the session if exists
-        HttpSession session = request.getSession(false);
-        LOGGER.info("User=" + session.getAttribute("email"));
-        if (session != null) {
-            session.invalidate();
-        }
-        request.getRequestDispatcher("logout.jsp").include(request, response);
     }
-
 }
